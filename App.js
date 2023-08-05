002d91fb039636984a08241734a1352e5e84fed3
ShivanshMehta <shivansh.mehta.aiml21@ggits.net>
2023-08-06 00:35:28 +0530

    form in native

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
@@ -7,23 +6,68 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [display, setDisplay] = useState(false);
+
+  const clear =()=>{
+    setDisplay(false);
+    setName(" ");
+    setPassword(" ");
+    setEmail(" ");
+    setPhone(" ");
+  }
 
   return (
-    <View style={{padding:10}}>
+    <View style={{ padding: 10 }}>
       <Text style={{ fontSize: 30, marginTop: 50, textAlign: "center" }}>
-        Handling Text Input
-      </Text>
-      <Text style={{ fontSize: 30, margin: 10, padding: 10 }}>
-        Your Name: {name}
+        Simple Form
       </Text>
       <TextInput
+        placeholder="Enter User Name"
         style={styles.textInput}
-        placeholder="Enter Your Name"
         value={name}
         onChangeText={(text) => setName(text)}
       />
-      <Button title="Clear Name" onPress={()=>setName(' ')} />
-      <StatusBar />
+      <TextInput
+        placeholder="Enter Password"
+        style={styles.textInput}
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <TextInput
+        placeholder="Enter E-Mail"
+        style={styles.textInput}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        placeholder="Enter Contact Info"
+        style={styles.textInput}
+        value={phone}
+        onChangeText={(text) => setPhone(text)}
+      />
+
+      <View style={{ margin: 10 }}>
+        <Button color={"green"} title="Submit" onPress={()=>setDisplay(true)} />
+      </View>
+      <View style={{ margin: 10 }}>
+        <Button color={"red"} title="Clear" onPress={clear} />
+      </View>
+      <View>
+        {
+          display?
+            <View>
+              <Text style={{ fontSize: 30, marginTop: 50, textAlign: "center" }}>UserName:{name}</Text>
+              <Text style={{ fontSize: 30, marginTop: 50, textAlign: "center" }}>Email:{email}</Text>
+              <Text style={{ fontSize: 30, marginTop: 50, textAlign: "center" }}>Contact:{phone}</Text>
+              
+            </View> : null
+        }
+      </View>
     </View>
   );
 }
