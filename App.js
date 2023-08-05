e316b0a9b6c890cff5608e674e131bb93997eee5
ShivanshMehta <shivansh.mehta.aiml21@ggits.net>
2023-08-06 00:59:47 +0530

    FLASTLIST

--- App.js
+++ App.js
@@ -1,76 +1,35 @@
-import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
-
-//external styling
-// import styles from "./styles.js/sampleStyle";
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [secPass, setSecPass] = useState(true);
-
-  const [display, setDisplay] = useState(false);
-
-  const clear =()=>{
-    setDisplay(false);
-    setName(" ");
-    setPassword(" ");
-    setEmail(" ");
-    setPhone(" ");
-  }
+  const users = [
+    {
+      id: 1,
+      name: "Shivansh",
+    },
+    {
+      id: 2,
+      name: "Hannu",
+    },
+    {
+      id: 3,
+      name: "Sample",
+    },
+    {
+      id:4,
+      name:'Sample1',
+    },
+  ];
 
   return (
     <View style={{ padding: 10 }}>
-      <Text style={{ fontSize: 30, marginTop: 50, textAlign: "center" }}>
-        Simple Form
-      </Text>
-      <TextInput
-        placeholder="Enter User Name"
-        style={styles.textInput}
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      <TextInput
-        placeholder="Enter Password"
-        style={styles.textInput}
-        secureTextEntry={secPass}
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
-              <Button title="show password" onPress={()=>setSecPass(false)}/>
-
-      <TextInput
-        placeholder="Enter E-Mail"
-        style={styles.textInput}
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        placeholder="Enter Contact Info"
-        style={styles.textInput}
-        value={phone}
-        onChangeText={(text) => setPhone(text)}
-      />
-
-      <View style={{ margin: 10 }}>
-        <Button color={"green"} title="Submit" onPress={()=>setDisplay(true)} />
-      </View>
-      <View style={{ margin: 10 }}>
-        <Button color={"red"} title="Clear" onPress={clear} />
-      </View>
-      <View>
-        {
-          display?
-            <View>
-              <Text style={{ fontSize: 30, marginTop: 50, textAlign: "center" }}>UserName:{name}</Text>
-              <Text style={{ fontSize: 30, marginTop: 50, textAlign: "center" }}>Email:{email}</Text>
-              <Text style={{ fontSize: 30, marginTop: 50, textAlign: "center" }}>Contact:{phone}</Text>
-              
-            </View> : null
+      <Text style={{ fontSize: 30, marginTop: 50, textAlign: "center" }}>Flat List Component</Text>
+      <FlatList
+        data={users}
+        renderItem={
+          ({item})=><Text>{item.id} and {item.name}</Text>
         }
-      </View>
+        keyExtractor={item =>item.id}
+      />
     </View>
   );
 }
