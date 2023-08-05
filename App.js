7fd184d830c4d62773c81c173bb0e26fab132aed
ShivanshMehta <shivansh.mehta.aiml21@ggits.net>
2023-08-06 00:06:31 +0530

    handling text input

--- App.js
+++ App.js
@@ -1,26 +1,40 @@
+import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 //external styling
-import styles from "./styles.js/sampleStyle";
+// import styles from "./styles.js/sampleStyle";
 
 export default function App() {
-  // let name1= 'Anil';
-  const [name, setName] = useState("Shivansh");
-  const change = () => {
-    setName("hannu hehe");
-  };
+  const [name, setName] = useState("");
+
   return (
-/* internal style */
-    <View style={styles.container}>
-      {/* inline styling */}
-      <Text style={{ fontSize: 20 }}>Styling in React Native</Text>
-      <Text style={{ fontSize: 20 }}>Styling in React Native</Text>
-      <Text style={{ fontSize: 20 }}>Styling in React Native</Text>
-      <Text style={{ fontSize: 20 }}>Styling in React Native</Text>
-      
+    <View style={{padding:10}}>
+      <Text style={{ fontSize: 30, marginTop: 50, textAlign: "center" }}>
+        Handling Text Input
+      </Text>
+      <Text style={{ fontSize: 30, margin: 10, padding: 10 }}>
+        Your Name: {name}
+      </Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Enter Your Name"
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+      <Button title="Clear Name" onPress={()=>setName(' ')} />
+      <StatusBar />
     </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  textInput: {
+    fontSize: 20,
+    color: "blue",
+    borderWidth: 3,
+    borderColor: "red",
+    margin: 10,
+    padding: 10,
+  },
+});
