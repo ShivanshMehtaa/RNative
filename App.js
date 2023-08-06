95443e0a555f8f65ac503c0df665c99a14c684c0
ShivanshMehta <shivansh.mehta.aiml21@ggits.net>
2023-08-06 20:38:29 +0530

    section list

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   Button,
   TextInput,
   FlatList,
+  SectionList,
 } from "react-native";
 
 export default function App() {
@@ -12,41 +13,51 @@ export default function App() {
     {
       id: 1,
       name: "Shivansh",
-      email:'test@abc.com'
+      email: 'test@abc.com',
+      data: ['PHP', 'React', 'Angular']
     },
     {
       id: 2,
       name: "Hannu",
-      email:'test@abc.com'
+      email: 'test@abc.com',
+      data: ['PHP', 'Test', 'Native']
     },
     {
       id: 3,
       name: "Sample",
-      email:'test@abc.com'
+      email: 'test@abc.com',
+      data: ['PHP', 'Python', 'CPP']
     },
     {
       id: 4,
       name: "Sample1",
-      email:'test@abc.com'
+      email: 'test@abc.com',
+      data: ['Java', 'NODE', 'MONGO']
     }
   ];
 
   return (
     <View style={{ padding: 10 }}>
-      <Text style={{ fontSize: 30, marginTop: 50,marginBottom:20, textAlign: "center" }}>
-        Custom flat list component
+      <Text style={{ fontSize: 30, marginTop: 50, marginBottom: 20, textAlign: "center" }}>
+        Section List in R-Native
       </Text>
-      <FlatList
-        data={users}
-        renderItem={({item})=>
-          <UserData item={item}/>
-      }
+      <SectionList
+        sections={users}
+        renderItem={({ item }) =>
+          <Text style={styles.text}>{item}</Text>
+        }
+        renderSectionHeader={({ section: { name } }) =>(
+          <Text style={styles.text}>{name}:-</Text>
+        )
+          
+        }
       />
+
     </View>
   );
 }
 
-const UserData = (props) =>{
+const UserData = (props) => {
   const vari = props.item
   return (
     <View style={styles.view}>
@@ -57,16 +68,16 @@ const UserData = (props) =>{
 }
 
 const styles = StyleSheet.create({
-  view:{
-    flex:1,
-    flexDirection:'row',
-    borderColor:'blue',
-    borderWidth:5,
-    marginBottom:10,
-    padding:10,
+  view: {
+    flex: 1,
+    flexDirection: 'row',
+    borderColor: 'blue',
+    borderWidth: 5,
+    marginBottom: 10,
+    padding: 10,
   },
   text: {
-    flex:1,
+    flex: 1,
     fontSize: 25,
     backgroundColor: "red",
     color: "white",
