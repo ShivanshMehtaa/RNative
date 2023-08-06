993279ec79d7dc95e6481430547edafa93071952
ShivanshMehta <shivansh.mehta.aiml21@ggits.net>
2023-08-06 17:14:38 +0530

    grid dynamics

--- App.js
+++ App.js
@@ -18,29 +18,69 @@ export default function App() {
       id:4,
       name:'Sample1',
     },
+    {
+      id:5,
+      name:'Sample1',
+    },
+    {
+      id:6,
+      name:'Sample1',
+    },
+    {
+      id:7,
+      name:'Sample1',
+    },
+    {
+      id:8,
+      name:'Sample1',
+    },
+    {
+      id:9,
+      name:'Sample1',
+    },
+    {
+      id:10,
+      name:'Sample1',
+    },
+    {
+      id:11,
+      name:'Sample1',
+    },
+    {
+      id:12,
+      name:'Sample1',
+    },
+    {
+      id:13,
+      name:'Sample1',
+    },
   ];
 
   return (
     <View style={{ padding: 10 }}>
-      <Text style={{ fontSize: 30, marginTop: 50, textAlign: "center" }}>Flat List Component</Text>
-      <FlatList
-        data={users}
-        renderItem={
-          ({item})=><Text>{item.id} and {item.name}</Text>
+      <Text style={{ fontSize: 30, marginTop: 50, textAlign: "center" }}>Grid in R-Native</Text>
+      <View style={{flex:1, flexDirection:'row',flexWrap:'wrap'}}>
+        {
+          users.map(
+            (item)=><Text style={styles.text}>{item.name}</Text>
+          )
         }
-        keyExtractor={item =>item.id}
-      />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  textInput: {
-    fontSize: 20,
-    color: "blue",
-    borderWidth: 3,
-    borderColor: "red",
-    margin: 10,
-    padding: 10,
-  },
+  text:{
+    fontSize:25,
+    backgroundColor:'red',
+    color:'white',
+    margin:10,
+    padding:10,
+    width:100,
+    height:100,
+    textAlignVertical:'center',
+    textAlign:'center',
+    borderRadius:20,
+  }
 });
