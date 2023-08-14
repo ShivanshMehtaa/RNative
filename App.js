b446ca265d11f8a8f06dabd29122fc6e7d13fcc7
ShivanshMehta <shivansh.mehta.aiml21@ggits.net>
2023-08-14 19:27:56 +0530

    useRef hook

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useRef } from "react";
 import {
   Button,
   FlatList,
@@ -7,75 +7,29 @@ import {
   Text,
   TextInput,
   View,
+  Modal
 } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("");
-  const [data, setData] = useState([]);
-  const [age, setAge] = useState("");
-  const [email, setEmail] = useState("");
 
-  const [nameError, setNameError] = useState(false);
-  const [ageError, setAgeError] = useState(false);
-  const [emailError, setEmailError] = useState(false);
+  const input = useRef();
 
-  async function getAPIData() {
-    const url = "http://10.0.2.2:3000/users";
-    let result = await fetch(url);
-    result = await result.json();
-    if (result) {
-      setData(result);
-    }
-  }
-  const deleteUser = async (id) =>{
-    const url = "http://10.0.2.2:3000/users";
-    // console.warn(`${url}/${id}`)
-
-    let result = await fetch(`${url}/${id}`,{
-      method:'delete'
-    });
-    result = await result.json();
-    if(result){
-      alert("User Deleted")
-    }
-
+  const updateButton = () =>{
+    console.warn('button cllick');
+    input.current.focus();
+    input.current.setNativeProps({
+      fontSize:24,
+      color:"red"
+    })
   }
 
-  useEffect(() => {
-    getAPIData();
-  }, [data]);
 
   return (
     <ScrollView style={styles.container}>
-      <View style={styles.dataWrapper}>
-        <View style={{flex:1.4}}>
-          <Text>Name</Text>
-        </View>
-        <View style={{flex:1.8}}>
-          <Text>Age</Text>
-        </View>
-        <View style={{flex:1}}>
-          <Text>Operations</Text>
-        </View>
-      </View>
-      {data.length
-        ? data.map((item) => (
-            <View style={styles.dataWrapper}>
-              <View style={styles.internal}>
-                <Text>{item.name}</Text>
-              </View>
-              <View style={{flex:0.6}}>
-                <Text>{item.age}</Text>
-              </View>
-              <View style={styles.internal}>
-                <Button onPress={()=>deleteUser(item.id)} title="delete" />
-              </View>
-              <View style={styles.internal}>
-                <Button title="update" />
-              </View>
-            </View>
-          ))
-        : null}
+      <TextInput ref={input} style={styles.inpBox} placeholder="enter name"/>
+      <TextInput style={styles.inpBox}placeholder="enter password"/>
+      <Button onPress={updateButton} title="Update Input"/>
+
     </ScrollView>
   );
 }
@@ -85,11 +39,20 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 50,
     borderRadius: 10,
+    padding:10,
   },
   dataWrapper: {
     flexDirection: "row",
     justifyContent: "space-around",
     backgroundColor: "orange",
+    borderRadius:10,
+    margin: 5,
+    padding: 10,
+  },
+  dataWrapper2: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: "skyblue",
     borderRadius:10,
     margin: 5,
     padding: 10,
@@ -101,4 +64,24 @@ const styles = StyleSheet.create({
   button: {
     marginRight: 3,
   },
+  modalStyle:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  modalView:{
+    backgroundColor:"#fff",
+    padding:30,
+    borderRadius:10,
+    shadowColor:'#000',
+    shadowOpacity:0.80,
+    elevation:5,  
+  },
+  inpBox:{
+    margin:10,
+    borderWidth:2,
+    borderRadius:5,
+    borderColor:'skyblue',
+    padding:5,
+  }
 });
